312575262faa6962f708658275e383bec5bc188a
alaabadra <alaabadra4@gmail.com>
2019-03-20 19:19:39 +0200

    setup server

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const home = require('./home');
-
+const {validation} = require('./middleware/validation.js');
+const {cookie} = require('./middleware/cookie.js');
+const {auth} = require('./middleware/authuntication.js');
+const {hashPasword} = require('./middleware/hashPassword.js');
 router.get('/', home.get);
+// router.post('/signup',validation,token,auth,hashPasword)
 
 module.exports = router;
---
+++ src/controller/middleware/validation.js
@@ -0,0 +1,31 @@
+
+// exports.signupValidation = (req,res)=>{
+//     const { userName,password ,confirmPassword}= req.body;
+// if(!userName.trim() || ! password.trim() || !confirmPassword){
+//     res.render('home',{
+//         msg:'you must fill all fields'
+//     });
+    
+// }
+// if(userName.includes(' ')){
+//     res.render('home',{
+//         msg:'you must do not spaces between char username'
+//     });
+// }
+// if(userName.length<5||userName.length>10){
+//     res.render('home',{
+//         msg:'you must length username on the less 5 and on the greater 10'
+//     });
+// }
+// if(password.length()<5||password.length()>10){
+//     res.render('home',{
+//         msg:'you must length password on the less 5 and on the greater 10'
+//     });
+// }
+// if(password.trim !== confirmPassword.trim()){
+//     res.render('home',{
+//         msg:'you must make sure password same confirm Password'
+//     });
+// }
+
+// }
